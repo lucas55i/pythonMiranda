e74f49eddb5d4cbce51b9b68742d289e757dbf06
Lucas Jesus <lucas.devj@outlook.com>
2023-06-19 22:32:29 -0300

    Comentários

---
+++ aula1.py
@@ -0,0 +1,13 @@
+# Permite escrever um comentário
+print(123)  # Na frente
+# Abaixo
+print(123)
+
+""" 
+DocString - o interpretador do python le
+Varias linhas para comentario
+"""
+
+'''
+Explicar suas notas
+'''
