7c89e3a76dc9a2579435a37af981fd59095bb4e9
Lucas Jesus <lucas.devj@outlook.com>
2023-06-19 22:58:47 -0300

    Introdução a str()

---
+++ aula3.py
@@ -0,0 +1,18 @@
+"""
+Python = Linguagem de programação
+Tipo de tipagem = Dinamica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+# Aspas simples
+print('Lucas -')
+print('Lucas "Silva"') # Aspas duplas dentro de aspas simples
+
+# Aspas Duplas
+print("Lucas --")
+
+# Escape
+print("Lucas \"Silva\"")
+
+# r
+print(r"Lucas \"Silva\"")
