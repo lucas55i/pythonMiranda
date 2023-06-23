cad0ea3c13a1e69051a1de7af9b72bb1e55922c8
Lucas Jesus <lucas.devj@outlook.com>
2023-06-22 21:28:29 -0300

    Introdução ao bool

---
+++ aula5.py
@@ -0,0 +1,11 @@
+# Tipo de dado bool (boolean)
+# AO questionar algo em um programa,
+# só existem duas respostas possíveis
+# sim (True) ou não (False)
+# Existem vários operadoes para questionar
+# Dentre eles o ==, que é um lógico que
+# questiona se um valor é igual a outro 
+print(10 == 10) # Sim => True (Verdadeiro)
+print(10 == 12) # Não => False (Falso)
+print(type(10 == 10))
+print(type(10 == 12))
