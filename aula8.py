1829360c844c798770b8aae375407c6fe863f84f
Lucas Jesus <lucas.devj@outlook.com>
2023-06-26 15:39:41 -0300

    primeiro exercicio

---
+++ aula8.py
@@ -0,0 +1,15 @@
+nome = 'Lucas'
+sobrenome = 'Silva Correa de Jesus'
+idade = 23
+ano_nascimento = 2023 - idade
+maior_de_idade = idade >= 18
+altura_metros = 1.81
+
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano da Nascimento:', ano_nascimento)
+print('Maior de Idade:', maior_de_idade)
+print('Altura Metros:', altura_metros)
+
