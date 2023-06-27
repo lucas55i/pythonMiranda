519e824d75c00d9b4336063bab0da603aed46344
Lucas Jesus <lucas.devj@outlook.com>
2023-06-27 15:45:49 -0300

    intro a input()

---
+++ aula15.py
@@ -0,0 +1,11 @@
+# nome = input('Qual o seu nome? ')
+# print(f'O seu nome é {nome=}')
+
+numero_1 = int(input('Digite um Número: '))
+numero_2 = int(input('Digite outro Número: '))
+
+int_num_1 = int(numero_1)
+int_num_2 = int(numero_2)
+
+
+print(f'A soma dos npumeros é: {int_num_1 + int_num_2}')
