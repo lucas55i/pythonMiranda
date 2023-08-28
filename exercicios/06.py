332a80322eca198426257294539cfb329bf9afd6
Lucas Silva <lucas.devj@outlook.com>
2023-08-27 21:42:05 -0300

    Correção ortografica.

--- exercicios/06.py
+++ exercicios/06.py
@@ -5,7 +5,7 @@ nome = "Lucas Silva"
 
 indice = 0
 novo_nome = ""
-while indice < len(nome): # itrando apartir do tamanho da string
+while indice < len(nome): # iterando apartir do tamanho da string
     letra = nome[indice]
     novo_nome += f"*{letra}"
     indice += 1
