1b44484a1d62031c3123f2fc41d877fe2a78e1fe
Lucas S C. Jesus <lucas_55i@outlook.com>
2024-04-14 22:51:40 -0300

    Renomeia arquivos

---
+++ modulo3/aula124.py
@@ -0,0 +1,43 @@
+# Mantendo estados dentro da classe
+class Camera:
+    def __init__(self, nome, filmando=False) -> None:
+        self.nome = nome
+        self.filmando = filmando
+
+    def filmar(self):
+        if self.filmando:
+            print(f"{self.nome} já está filmando...")
+            return
+
+        print(f"{self.nome} está filmando...")
+        self.filmando = True
+
+    def parar_filmar(self):
+        if not self.filmando:
+            print(f"{self.nome} não está filmando...")
+            return
+
+        print(f"{self.nome} está parando filmando...")
+        self.filmando = False
+
+    def fotografar(self):
+        if self.filmando:
+            print(f"{self.nome} não pode fotografar filmando...")
+            return
+
+        print(f"{self.nome} está fotografando...")
+
+
+c1 = Camera('Canon')
+c2 = Camera('Sony')
+
+c1.fotografar()
+c1.filmar()
+c1.parar_filmar()
+c1.parar_filmar()
+
+print("--------")
+c2.fotografar()
+c2.filmar()
+c2.parar_filmar()
+# c1.filmar()
--- modulo3/aula125.py
+++ modulo3/aula125.py
@@ -1,41 +1,13 @@
-# Mantendo estados dentro da classe
-class Camera:
-    def __init__(self, nome, filmando=False) -> None:
-        self.nome = nome
-        self.filmando = filmando
+# Atributos de classe
+class Pessoa:
+    ano_atual = 2023
 
-    def filmar(self):
-        if self.filmando:
-            print(f"{self.nome} já está filmando...")
-            return
+    def __init__(self, nome, idade) -> None:
+        self.nome =  nome
+        self.idade = idade
 
-        print(f"{self.nome} está filmando...")
-        self.filmando = True
+    def get_ano_nascimento(self):
+        return Pessoa.ano_atual - self.idade 
 
-    def parar_filmar(self):
-        if not self.filmando:
-            print(f"{self.nome} não está filmando...")
-            return
-
-        print(f"{self.nome} está parando filmando...")
-        self.filmando = False
-
-    def fotografar(self):
-        if self.filmando:
-            print(f"{self.nome} não pode fotografar filmando...")
-            return
-
-        print(f"{self.nome} está fotografando...")
-
-
-c1 = Camera('Canon')
-c2 = Camera('Sony')
-
-c1.fotografar()
-c1.filmar()
-c1.parar_filmar()
-print("--------")
-c2.fotografar()
-c2.filmar()
-c2.parar_filmar()
-# c1.filmar()
+p1 = Pessoa('João', 32)
+print(p1.get_ano_nascimento())
