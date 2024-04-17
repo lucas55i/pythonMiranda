043e4f368ed3e2589aedaafe49455e7bbf20b08d
Lucas S C.Jesus <lucas_55i@outlook.com>
2024-04-17 11:47:41 -0300

    @property + @setter

--- modulo3/aula130.py
+++ modulo3/aula130.py
@@ -2,17 +2,41 @@
 # - como getter
 # - p/ evitar quebrar código cliente
 # - p/ habilitar setter
-# - p/ executar ações ao obter um atributo
+# - p/ executar ações ao obter um 
+# Atributos que começar com um ou dois underlines
+# não devem ser usados fora da classe
 
 class Caneta:
     def __init__(self, cor):
-        self.cor_tinta = cor
+        self._cor = cor
+        self._cor_tampa = None
 
     @property
     def cor(self):
-        print("PROPERTY")
-        return self.cor_tinta
+        # print("PROPERTY")
+        return self._cor
+    
+    @cor.setter
+    def cor(self, valor):
+        # if valor == 'Rosa':
+        #     raise ValueError('Não aceito essa cor')
+        self._cor = valor
+
+    @property
+    def cor_tampa(self):
+        return self._cor_tampa
+
+    @cor_tampa.setter
+    def cor_tampa(self, valor):
+        self._cor_tampa = valor
+
+    
+def mostrar(caneta):
+    return caneta.cor
 
    
     
 caneta = Caneta('Azul')
+caneta.cor = 'Rosa-'
+caneta._cor_tampa = 'Vermelho'
+print(caneta.cor_tampa)
